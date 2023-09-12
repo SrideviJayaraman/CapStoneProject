1cbb4e0c4245898c099c140f97bc9bb5c268cd69
Sridevi <88590415+SrideviJayaraman@users.noreply.github.com>
2023-09-12 12:04:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,17 +2,17 @@ resource "aws_vpc" "sri_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "sub1(pub)" {
+resource "aws_subnet" "sub1_pub" {
   vpc_id     = aws_vpc.sri_vpc.id
   cidr_block = "10.0.1.0/24"
 }
 
-resource "aws_subnet" "sub2(pub)" {
+resource "aws_subnet" "sub2_pub" {
   vpc_id     = aws_vpc.sri_vpc.id
   cidr_block = "10.0.2.0/24"
 }
 
-resource "aws_subnet" "sub3(priv)" {
+resource "aws_subnet" "sub3_priv" {
   vpc_id     = aws_vpc.sri_vpc.id
   cidr_block = "10.0.3.0/24"
 }
