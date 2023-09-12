f6ae11991001f8c800f3aecd48e8cf1683ddf729
Sridevi <88590415+SrideviJayaraman@users.noreply.github.com>
2023-09-12 12:09:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,16 +3,16 @@ resource "aws_vpc" "sri-vpc" {
 }
 
 resource "aws_subnet" "sub1_pub" {
-  vpc_id     = aws_vpc.sri_vpc.id
+  vpc_id     = aws_vpc.sri-vpc.id
   cidr_block = "10.0.1.0/24"
 }
 
 resource "aws_subnet" "sub2_pub" {
-  vpc_id     = aws_vpc.sri_vpc.id
+  vpc_id     = aws_vpc.sri-vpc.id
   cidr_block = "10.0.2.0/24"
 }
 
 resource "aws_subnet" "sub3_priv" {
-  vpc_id     = aws_vpc.sri_vpc.id
+  vpc_id     = aws_vpc.sri-vpc.id
   cidr_block = "10.0.3.0/24"
 }
