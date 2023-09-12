824b8ae66edbfd20ea883c124aec82ffd4579095
Sridevi <88590415+SrideviJayaraman@users.noreply.github.com>
2023-09-12 12:56:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,14 +5,64 @@ resource "aws_vpc" "sri-vpc" {
 resource "aws_subnet" "sub1_pub" {
   vpc_id     = aws_vpc.sri-vpc.id
   cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "sub2_pub" {
   vpc_id     = aws_vpc.sri-vpc.id
   cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
 }
 
 resource "aws_subnet" "sub3_priv" {
   vpc_id     = aws_vpc.sri-vpc.id
   cidr_block = "10.0.3.0/24"
+  availability_zone = "us-east-1c"
+}
+
+resource "aws_route_table" "PUBLIC-RT" {
+  vpc_id = aws_vpc.sri-vpc.id
+}
+
+resource "aws_route_table" "PRIVATE-RT" {
+  vpc_id = aws_vpc.sri-vpc.id
+}
+
+resource "aws_route_table_association" "PUBLIC-RT" {
+  subnet_id      = aws_subnet.sub1_pub.id
+  subnet_id      = aws_subnet.sub2_pub.id
+  route_table_id = aws_route_table.PUBLIC-RT.id
+}
+
+resource "aws_route_table_association" "PRIVATE-RT" {
+  subnet_id      = aws_subnet.sub3_priv.id
+  route_table_id = aws_route_table.PRIVATE-RT.id
+}
+
+resource "aws_internet_gateway" "sri-gw" {
+  vpc_id = aws_vpc.sri-vpc.id
+}
+
+resource "aws_internet_gateway_attachment" "sri-gw-attach" {
+  internet_gateway_id = aws_internet_gateway.sri-gw.id
+  vpc_id              = aws_vpc.sri-vpc.id
+}
+
+resource "aws_route_table_association" "rt-igw" {
+  gateway_id     = aws_internet_gateway.sri-gw.id
+  route_table_id = aws_route_table.PUBLIC-RT.id
+}
+ resource "aws_eip" "sri-eip" {
+  domain   = "vpc"
+}
+
+resource "aws_nat_gateway" "sri-ngw" {
+  allocation_id = aws_eip.sri-eip.id
+  subnet_id     = aws_subnet.sub1_pub.id
+}
+
+resource "aws_route" "PRIVATE-RT-NGW" {
+  route_table_id         = aws_route_table.PRIVATE-RT.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.sri-ngw.id
 }
