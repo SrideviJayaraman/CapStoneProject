f4277a8e3b7b5aed681b8f27092c67214c831f13
Sridevi <88590415+SrideviJayaraman@users.noreply.github.com>
2023-09-12 23:35:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,11 +53,7 @@ resource "aws_nat_gateway" "CAPSTONE" {
   allocation_id = aws_eip.CAPSTONE.id
   subnet_id     = aws_subnet.PUBLIC-1.id
 }
-resource "aws_route" "ATTACH" {
-  route_table_id = aws_route_table.PRIVATE.id
-  destination_cidr_block    = "10.0.0.0/16"
-  nat_gateway_id = aws_nat_gateway.CAPSTONE.id
-}
+
 resource "aws_security_group" "CAPSTONE" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
@@ -99,17 +95,12 @@ resource "aws_security_group" "CAPSTONE" {
     Name = "allow_tls"
   }
 }
-resource "aws_key_pair" "CAPSTONE-KP" {
-  key_name   = "CAPSTONE-KP" 
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9AGgJBb/Z3Ll89GoPoG1QVh9tDbvuLa1PjUiYuPvbYVY5/BPp/ROkWcrwG4HpEzRMgb3UfV3N53upteRwbhZSsDvoI85eu/t9s7Yl+dyQZUadhDYoPMf2Dfu/UDPbAQdvV2D40vRsTlgXB6TxODsv5ebt6NfwUwb8DIqE9yo6renx7QJzDtR42zFws03oiGgD4R0HSX6jAoCjKNNwIOch13RDh10RftBI7lvKJB/j6VMVBkXW6m97J1R8ZEUZKtxfZpJ8fg9Y602agrnt9UviC7JJCL8dJmySfrBIY4JHFlhZmSidpFSgyE0AnAD25/4q77FlsSCtIrbjOEmsbOSt suru@suru"
-}
 
 resource "aws_instance" "CAPSTONE" {
   ami           = "ami-053b0d53c279acc90"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.CAPSTONE.id]
   subnet_id = aws_subnet.PRIVATE.id
-  key_name = aws_key_pair.CAPSTONE-KP.key_name
 
   tags = {
     Name = "HelloWorld"
