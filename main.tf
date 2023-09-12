4e64ec2f24331a16c0398d88c854b0f547c1ad86
Sridevi <88590415+SrideviJayaraman@users.noreply.github.com>
2023-09-12 23:03:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,66 +1,114 @@
-resource "aws_vpc" "sri-vpc" {
-  cidr_block = "10.0.0.0/16"
-}
+resource "aws_vpc" "CAPSTONE" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-resource "aws_subnet" "sub1_pub" {
-  vpc_id     = aws_vpc.sri-vpc.id
+  tags = {
+    Name = "CAPSTONE"
+  }
+}
+resource "aws_subnet" "PUBLIC-1" {
+  vpc_id     = aws_vpc.CAPSTONE.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-1a"
 }
-
-resource "aws_subnet" "sub2_pub" {
-  vpc_id     = aws_vpc.sri-vpc.id
+resource "aws_subnet" "PUBLIC-2" {
+  vpc_id     = aws_vpc.CAPSTONE.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-1b"
 }
-
-resource "aws_subnet" "sub3_priv" {
-  vpc_id     = aws_vpc.sri-vpc.id
+resource "aws_subnet" "PRIVATE" {
+  vpc_id     = aws_vpc.CAPSTONE.id
   cidr_block = "10.0.3.0/24"
   availability_zone = "us-east-1c"
 }
-
-resource "aws_route_table" "PUBLIC-RT" {
-  vpc_id = aws_vpc.sri-vpc.id
+resource "aws_route_table" "PUBLIC" {
+  vpc_id = aws_vpc.CAPSTONE.id
 }
-
-resource "aws_route_table" "PRIVATE-RT" {
-  vpc_id = aws_vpc.sri-vpc.id
+resource "aws_route_table" "PRIVATE" {
+  vpc_id = aws_vpc.CAPSTONE.id
 }
-
-resource "aws_route_table_association" "PUBLIC-RT1" {
-  subnet_id      = aws_subnet.sub1_pub.id
-  route_table_id = aws_route_table.PUBLIC-RT.id
+resource "aws_route_table_association" "PUBLIC-1" {
+  subnet_id      = aws_subnet.PUBLIC-1.id
+  route_table_id = aws_route_table.PUBLIC.id
 }
-
-resource "aws_route_table_association" "PUBLIC-RT2" {
-  subnet_id      = aws_subnet.sub2_pub.id
-  route_table_id = aws_route_table.PUBLIC-RT.id
+resource "aws_route_table_association" "PUBLIC-2" {
+  subnet_id      = aws_subnet.PUBLIC-2.id
+  route_table_id = aws_route_table.PUBLIC.id
 }
-
-resource "aws_route_table_association" "PRIVATE-RT" {
-  subnet_id      = aws_subnet.sub3_priv.id
-  route_table_id = aws_route_table.PRIVATE-RT.id
+resource "aws_route_table_association" "PRIVATE" {
+  subnet_id      = aws_subnet.PRIVATE.id
+  route_table_id = aws_route_table.PRIVATE.id
 }
-
-resource "aws_internet_gateway" "sri-gw" {
-  vpc_id = aws_vpc.sri-vpc.id
+resource "aws_internet_gateway" "CAPSTONE" {
+  vpc_id = aws_vpc.CAPSTONE.id
 }
-
-resource "aws_route_table_association" "rt-igw" {
-  gateway_id     = aws_internet_gateway.sri-gw.id
-  route_table_id = aws_route_table.PUBLIC-RT.id
+resource "aws_route_table_association" "IG-ROUTE" {
+  gateway_id     = aws_internet_gateway.CAPSTONE.id
+  route_table_id = aws_route_table.PUBLIC.id
 }
-resource "aws_eip" "sri-eip" {
+resource "aws_eip" "CAPSTONE" {
   domain   = "vpc"
 }
-
-resource "aws_nat_gateway" "sri-ngw" {
-  allocation_id = aws_eip.sri-eip.id
-  subnet_id     = aws_subnet.sub1_pub.id
+resource "aws_nat_gateway" "CAPSTONE" {
+  allocation_id = aws_eip.CAPSTONE.id
+  subnet_id     = aws_subnet.PUBLIC-1.id
 }
+resource "aws_route" "ATTACH" {
+  route_table_id = aws_route_table.PRIVATE.id
+  nat_gateway_id = aws_nat_gateway.CAPSTONE.id
+}
+resource "aws_security_group" "CAPSTONE" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.CAPSTONE.id
 
-resource "aws_route" "PRIVATE-RT-NGW" {
-  route_table_id         = aws_route_table.PRIVATE-RT.id
-  nat_gateway_id         = aws_nat_gateway.sri-ngw.id
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.CAPSTONE.cidr_block]
+  }
+
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.CAPSTONE.cidr_block]
+  }
+
+    ingress {
+    description      = "TLS from VPC"
+    from_port        = 3000
+    to_port          = 3000
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.CAPSTONE.cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+resource "aws_key_pair" "CAPSTONE-KP" {
+  key_name = "CAPSTONE.pem"
+}
+resource "aws_instance" "CAPSTONE-PROJECT" {
+  ami           = ami-053b0d53c279acc90
+  instance_type = "t2.micro"
+  vpc_security_group_ids = aws_security_group.CAPSTONE.id
+  subnet_id = aws_subnet.PRIVATE.id
+  key_name = aws_key_pair.CAPSTONE.CAPSTONE.pem
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
