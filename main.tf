f25930f74d63abef66ee64bb24530f7b90aa7741
Sridevi <88590415+SrideviJayaraman@users.noreply.github.com>
2023-09-13 18:37:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,136 +1,176 @@
-resource "aws_vpc" "CAPSTONE" {
-  cidr_block       = "10.0.0.0/16"
-  instance_tenancy = "default"
-
+# vpc
+resource "aws_vpc" "sri_vpc" {
+  cidr_block = var.vpc_cidr
   tags = {
-    Name = "CAPSTONE"
+    Name = "sri-terr-vpc"
   }
 }
-resource "aws_subnet" "PUBLIC-1" {
-  vpc_id     = aws_vpc.CAPSTONE.id
-  cidr_block = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+# subnet 1 (public)
+resource "aws_subnet" "sri_subnet1" {
+  vpc_id = aws_vpc.sri_vpc.id
+  cidr_block = var.subnet1_cidr
+  availability_zone_id = var.subnet1_availability_zone
+  map_public_ip_on_launch = true
   tags = {
-    Name = "PUBLIC-1"
-  }
-}
-resource "aws_subnet" "PUBLIC-2" {
-  vpc_id     = aws_vpc.CAPSTONE.id
-  cidr_block = "10.0.2.0/24"
-  availability_zone = "us-east-1b"
-  tags = {
-    Name = "PUBLIC-2"
-  }
-}
-resource "aws_subnet" "PRIVATE" {
-  vpc_id     = aws_vpc.CAPSTONE.id
-  cidr_block = "10.0.3.0/24"
-  availability_zone = "us-east-1c"
-  tags = {
-    Name = "PRIVATE"
-  }
-}
-resource "aws_route_table" "PUBLIC" {
-  vpc_id = aws_vpc.CAPSTONE.id
-  tags = {
-    Name = "PUBLIC"
-  }
-}
-resource "aws_route_table" "PRIVATE" {
-  vpc_id = aws_vpc.CAPSTONE.id
-    route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.CAPSTONE.id
-  }
-  tags = {
-    Name = "PRIVATE"
-  }
-}
-resource "aws_route_table_association" "PUBLIC-1" {
-  subnet_id      = aws_subnet.PUBLIC-1.id
-  route_table_id = aws_route_table.PUBLIC.id
-}
-resource "aws_route_table_association" "PUBLIC-2" {
-  subnet_id      = aws_subnet.PUBLIC-2.id
-  route_table_id = aws_route_table.PUBLIC.id
-}
-resource "aws_route_table_association" "PRIVATE" {
-  subnet_id      = aws_subnet.PRIVATE.id
-  route_table_id = aws_route_table.PRIVATE.id
-}
-resource "aws_internet_gateway" "CAPSTONE" {
-  vpc_id = aws_vpc.CAPSTONE.id
-  tags = {
-    Name = "CAPSTONE"
-  }
-}
-resource "aws_route_table_association" "IG-ROUTE" {
-  gateway_id     = aws_internet_gateway.CAPSTONE.id
-  route_table_id = aws_route_table.PUBLIC.id
-}
-resource "aws_eip" "CAPSTONE" {
-  domain   = "vpc"
-  tags = {
-    Name = "CAPSTONE"
-  }
-}
-resource "aws_nat_gateway" "CAPSTONE" {
-  allocation_id = aws_eip.CAPSTONE.id
-  subnet_id     = aws_subnet.PUBLIC-1.id
-  tags = {
-    Name = "CAPSTONE"
+    Name = "Terr-subnet1"
   }
 }
 
-resource "aws_security_group" "CAPSTONE" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.CAPSTONE.id
+resource "aws_subnet" "sri_subnet2" {
+  vpc_id = aws_vpc.sri_vpc.id
+  cidr_block = var.subnet2_cidr
+  availability_zone_id = var.subnet2_availability_zone
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "Terr-subnet2"
+  }
+}
+# subnet 2(public) change it to private
+resource "aws_subnet" "sri_subnet3" {
+  vpc_id = aws_vpc.sri_vpc.id
+  cidr_block = var.subnet3_cidr
+  availability_zone_id = var.subnet3_availability_zone
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "Terr-subnet3"
+  }
 
+}
+
+# internet gateway
+resource "aws_internet_gateway" "sri_igw" {
+  vpc_id = aws_vpc.sri_vpc.id
+  tags = {
+    Name = "Terr-igw"
+  }
+}
+
+
+# route table 1 
+resource "aws_route_table" "sri_rt_public" {
+  vpc_id = aws_vpc.sri_vpc.id
+  tags = {
+    Name = "Terr-rt1"
+  }
+}
+#route table 2 
+resource "aws_route_table" "sri_rt_private" {
+  vpc_id = aws_vpc.sri_vpc.id
+  tags = {
+    Name = "Terr-rt2"
+  }
+}
+# Associating route table 1 with subnet 1 
+resource "aws_route_table_association" "sri_subnet_rt_association1" {
+  subnet_id = aws_subnet.sri_subnet1.id
+  route_table_id = aws_route_table.sri_rt_public.id
+}
+
+# Associating route table 1 with subnet 2
+resource "aws_route_table_association" "sri_subnet_rt_association2" {
+  subnet_id = aws_subnet.sri_subnet2.id
+  route_table_id = aws_route_table.sri_rt_public.id
+}
+
+# Associating route table 2 with subnet 3
+resource "aws_route_table_association" "sri_subnet_rt_association3" {
+  subnet_id = aws_subnet.sri_subnet3.id
+  route_table_id = aws_route_table.sri_rt_private.id
+}
+
+# Adding the internet gateway route to route table 1
+resource "aws_route" "sri_route1" {
+  route_table_id = aws_route_table.sri_rt_public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.sri_igw.id
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "sri_nat_gateway" {
+  allocation_id = aws_eip.sri_eip.id
+  subnet_id     = aws_subnet.sri_subnet1.id # Replace with your public subnet ID
+}
+
+# Elastic IP for NAT Gateway
+resource "aws_eip" "sri_eip" {
+  vpc = true
+}
+
+# Modify the route table associated with your private subnet to route traffic through the NAT Gateway
+resource "aws_route" "sri_private_subnet_route" {
+  route_table_id         = aws_route_table.sri_rt_private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.sri_nat_gateway.id
+}
+
+# creating a security group
+resource "aws_security_group" "sri_sg" {
+  name = "terr-securitygroup"
+  vpc_id = aws_vpc.sri_vpc.id
   ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.CAPSTONE.cidr_block]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.CAPSTONE.cidr_block]
+  ingress {
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
-    ingress {
-    description      = "TLS from VPC"
-    from_port        = 3000
-    to_port          = 3000
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.CAPSTONE.cidr_block]
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "CAPSTONE"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-resource "aws_instance" "CAPSTONE" {
-  ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.CAPSTONE.id]
-  subnet_id = aws_subnet.PRIVATE.id
-
+# Create EC2 instances
+resource "aws_instance" "web_server1" {
+  ami           = var.ami_id 
+  instance_type = var.instance_type     
+  subnet_id     = aws_subnet.sri_subnet1.id
+  key_name      = "ssh-terraform-key"
+  security_groups = [aws_security_group.sri_sg.id]
   tags = {
-    Name = "CAPSTONE"
+    Name = "webserver-public"
   }
+}
+resource "aws_instance" "web_server2" {
+  ami           = var.ami_id 
+  instance_type = var.instance_type     
+  subnet_id     = aws_subnet.sri_subnet2.id
+  key_name      = "ssh-terraform-key"
+  security_groups = [aws_security_group.sri_sg.id]
+  tags = {
+    Name = "webserver-private"
+  }
+}
+
+output "private_ip1" {
+   value = aws_instance.web_server1.private_ip
+}
+output "public_ip1" {
+   value = aws_instance.web_server1.public_ip
+}
+
+output "private_ip2" {
+   value = aws_instance.web_server2.private_ip
+}
+output "public_ip2" {
+   value = aws_instance.web_server2.public_ip
 }
